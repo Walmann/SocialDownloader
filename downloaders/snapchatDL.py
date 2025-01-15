5ab0b71f8e5d54fe362ce555e3b7b1484682443c
Walmann <tovare@walmann.it>
2025-01-15 09:03:20 +0100

    Changed way to find username in link

--- downloaders/snapchatDL.py
+++ downloaders/snapchatDL.py
@@ -1,11 +1,17 @@
 import snapchat_dl
 import time
 import requests
+import re
+
 
 from dep.profileClass import profileClass
 
 def profileDownload(profile: profileClass):
-    profileSnapUsername = profile.link.strip("https://www.snapchat.com/add/").strip("http://www.snapchat.com/add/")
+    
+    profileSnapUsername = findUsername(profile.link)
+
+
+    # profileSnapUsername = profile.link.strip("https://www.snapchat.com/add/").strip("http://www.snapchat.com/add/")
     download(profile=profileSnapUsername, downloadDirectory=profile.downloadLocation)
     pass
 
@@ -28,3 +34,17 @@ def download(profile, downloadDirectory):
         print("User not found! Continuing")
         pass
     pass
+
+
+def findUsername(lunk):
+    # Regex for å finne brukernavn
+    regex = r"https?://www\.snapchat\.com/add/([^/?]+)"
+
+    
+    match = re.search(regex, lunk)
+    if match:
+        brukernavn = match.group(1)
+        return brukernavn
+    else:
+        raise ValueError(f"Fant ingen brukernavn i {lunk}")
+        
